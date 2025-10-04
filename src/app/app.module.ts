66aca5a9f7ce40d1374c1edd6a06e80a595ed633
Dulani Lamahewage <95127130+lamahewageDulani@users.noreply.github.com>
2025-10-04 10:31:56 +0530

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EmployeeMasterComponent } from './masters/employee-master/employee-master.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmployeeMasterComponent
   ],
   imports: [
     BrowserModule,
