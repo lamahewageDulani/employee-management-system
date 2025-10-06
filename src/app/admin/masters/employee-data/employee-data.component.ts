9e1a2631dc8c70235c5b8f38a9cd7220e9860a2e
Dulani Lamahewage <95127130+lamahewageDulani@users.noreply.github.com>
2025-10-06 20:28:28 +0530

    employee add component frontend without validations

--- src/app/admin/masters/employee-data/employee-data.component.ts
+++ src/app/admin/masters/employee-data/employee-data.component.ts
@@ -1,11 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { CommonModalComponent } from '../../../components/common-modal/common-modal.component';
+import { AddEmployeeComponent } from './add-employee/add-employee.component';
 
 @Component({
   selector: 'app-employee-data',
-  imports: [],
+  imports: [
+    CommonModalComponent,
+    AddEmployeeComponent
+  ],
   templateUrl: './employee-data.component.html',
   styleUrl: './employee-data.component.css'
 })
+
 export class EmployeeDataComponent {
 
+  showEmployeeModal = false;
+
+  openAddModal() {
+    this.showEmployeeModal = true;
+    console.log('Add Employee Modal opened');
+  }
+
+  saveEmployee() {
+    console.log('Saving employee...');
+    // you can access AddEmployeeComponent data later using @ViewChild
+    this.showEmployeeModal = false;
+  }
 }
---
+++ src/app/components/common-modal/common-modal.component.ts
@@ -0,0 +1,27 @@
+import { CommonModule } from '@angular/common';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-common-modal',
+  imports: [ CommonModule ],
+  templateUrl: './common-modal.component.html',
+  styleUrl: './common-modal.component.css'
+})
+export class CommonModalComponent {
+  @Input() title: string = '';
+  @Input() showSave: boolean = true;
+  @Input() visible: boolean = false;   // control modal open/close
+  @Output() save = new EventEmitter<void>();
+  @Output() close = new EventEmitter<void>();
+
+  onSave() {
+    this.save.emit();
+    this.visible = false;
+  }
+
+  onClose() {
+    this.close.emit();
+    this.visible = false;
+  }
+
+}
